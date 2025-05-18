b29e9493a72ac9327f2d17014e8efefaf8d17ef6
CelinaPs <143390437+CelinaPs@users.noreply.github.com>
2025-05-18 23:12:51 +0200

    Litt kommentarer

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,14 +1,14 @@
 export default function EventCard({ event, isWishlisted, onToggleWishlist }) {
-  const image = event.images?.[0]?.url;
+  const image = event.images?.[0]?.url; //En const som prøver å hente bilde-url fra event.images, ? sjekker at "images" finnes først
 
   return (
-    <li className="event-card">
-      {<img src={image} alt={event.name} width="200" />}
+    <li className="event-card"> {/*Bare navn på komponenten*/}
+      {<img src={image} alt={event.name} width="200" />} {/*Viser bildet, lager alternativ tekst*/}
       
       <h3>{event.name}</h3> {/* Viser navn på event */}
       
       {/*Sette icon under bilde kilde: https://legacy.reactjs.org/docs/handling-events.html */}
-      <button onClick={() => onToggleWishlist(event)}>
+      <button onClick={() => onToggleWishlist(event)}> {/*Når knappen trykkes skjer endring*/}
         {isWishlisted ? (
           <i className="fa-solid fa-heart"></i>
         ) : (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ export default function Nav({ isLoggedIn }) {
   return (
     <nav className="navigation">
       <p className="logo-home">
-        <Link to="/">BillettLyst</Link>
+        <Link to="/">BillettLyst</Link> {/*Link til hjemmesiden*/}
       </p>
       <p className="categories">
-        <Link to="/category/musikk">Musikk</Link>
-        <Link to="/category/sport">Sport</Link>
-        <Link to="/category/teater">Teater/Show</Link>
+        <Link to="/category/musikk">Musikk</Link> {/*Link til categorypage musik*/}
+        <Link to="/category/sport">Sport</Link> {/*Link til categorypage sport*/}
+        <Link to="/category/teater">Teater/Show</Link> {/*Link til categorypage teater*/}
       </p>
       <p>
         {/*Se om brukeren er logget inn eller ikke, velger tittel ut i fra det. */}
